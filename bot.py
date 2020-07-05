a44bcd8c5f41f0f8943c90e476c871d3435b3116
ChiyoOsaka <github@chiyo-chan.me>
2020-07-05 18:55:08 +0100

    fixing some bugs and more progress

--- bot.py
+++ bot.py
@@ -1,23 +1,26 @@
 import discord
 from discord.ext import commands
-from bot import bot
-import imgs, vars
 bot = commands.Bot(command_prefix='++')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(':stopwatch: Pong!')
 
-@bot.command()
-async def help(ctx):
-    embed=discord.Embed(title="Command list", description="**Here is a list of all the commands:**", color=#2ECC71)
+@bot.command(aliases=['commands'])
+async def help2(ctx):
+    embed=discord.Embed(title="Command list", description="**Here is a list of all the commands:**", color=0x2ECC71)
     embed.add_field(name="Image commands:", value="foobar", inline=True)
     embed.add_field(name="Misc. commands", value="+help\n+sourcecode", inline=True)
     await ctx.send(embed=embed)
 
 @bot.command(aliases=['source', 'code'])
 async def sourcecode(ctx):
+    embed=discord.Embed(title="The bot's source code", color=0x2ECC71,
+                        description="The source code for this bot is open source and is available on Github.")
+    await ctx.send(embed=embed)
 
-
+@bot.command(aliases=['bug', 'bugs', 'suggest'])
+async def suggestions(ctx):
+    pass
 
 bot.run('xxx')
