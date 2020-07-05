be8d4c0d6a16895199cdf675dd0573d625004831
ChiyoOsaka <github@chiyo-chan.me>
2020-07-05 18:07:00 +0100

    some WIP for other commands

--- bot.py
+++ bot.py
@@ -1,2 +1,16 @@
 from discord.ext import commands
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='++')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(':stopwatch: Pong!')
+
+@bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="Command list", description="**Here is a list of all the commands:**", color=#2ECC71)
+    embed.add_field(name="Image commands:", value="foobar", inline=True)
+    embed.add_field(name="Misc. commands", value="+help\n+sourcecode", inline=True)
+    await ctx.send(embed=embed)
+
+@bot.command(aliases=['source', 'code'])
+async def sourcecode(ctx):
