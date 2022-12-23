b87d98485268e90f8fc82e6902bb56ea3e21807e
KanchiMoe <github@chiyo-chan.me>
2022-12-23 21:48:42 +0000

    add/complete role validation

--- autopromote.py
+++ autopromote.py
@@ -2,37 +2,52 @@ import discord
 
 import global_objects as GO
 
-AB_SERVER = 933781308289712178 
+AB_SERVER_ID = 933781308289712178
 
 ROLE_IDS = {
-    'aahq': 933187649143980064,
-    'bf': 933187568944685076,
-    'guild': 1008455039884468244,
-    'recruit': 933187488053334022,
+    'AAHQ': 933187649143980064,
+    'Battlefront': 933187568944685076,
+    'Guild': 1008455039884468244,
+    'Recruit': 933187488053334022,
 }
 
-START_MSG = "Starting Autopromotions. Outputting logs to channel ID `{log_channel_id}` (<#{log_channel_id}>)."
-START_ERR = ":x: The ID you provided (`{log_channel_id}`) contains letters, symbols or spaces. Only numbers are allowed."
+MSGS = {
+    'init': "Starting Autopromotions. Outputting logs to channel ID `{log_channel_id}` (<#{log_channel_id}>).",
+    'idnotint': ":x: The ID you provided (`{log_channel_id}`) contains letters, symbols or spaces. Only numbers are allowed.",
+    'start': ":robot: | Autopromotions started by <@{interaction.user.id}>. Conducting start-up validation checks.",
+    'rolecheck': ":mag: | Checking for AAHQ, Battlefront, Guild and Recruit roles.",
+    'rolenotexist': ':x: | Role "{role_name}" does not exist.',
+    'roleexists': ':white_check_mark: | Role "{role_name}" exists.',
+    'createrole': ' Please double check the role ID used in the code. ID: `{role_id}`'
+}
 
 @GO.BOT_CLIENT.tree.command(name="apwip",description="Promote users in Angel Beats! based on the most recent promotion data.")
 async def apwip(interaction: discord.Interaction, log_channel_id: str):
-    
-    # Doing this cause https://github.com/Rapptz/discord.py/issues/9147
+
+    log_channel = await GetLogChannelObj(interaction, log_channel_id)
+    await interaction.response.send_message(MSGS['init'].format(**locals(), **globals()))
+
+    await log_channel.send(f"{MSGS['start']}".format(**locals(), **globals()))
+    await log_channel.send(f"{MSGS['rolecheck']}".format(**locals(), **globals()))
+
+    await CheckRolesExist(log_channel)
+
+# Doing this cause https://github.com/Rapptz/discord.py/issues/9147
+async def GetLogChannelObj(interaction: discord.Interaction, log_channel_id: str):
     if not log_channel_id.isnumeric():
-        await interaction.response.send_message(START_ERR.format(**locals(), **globals()))
+        await interaction.response.send_message(MSGS['idnotint'].format(**locals(), **globals()))
+        raise ValueError("foobar")
     else:
-        await interaction.response.send_message(START_MSG.format(**locals(), **globals()))
+        return GO.BOT_CLIENT.get_channel(int(log_channel_id)) 
 
-    log_channel = GO.BOT_CLIENT.get_channel(int(log_channel_id))
-    await log_channel.send(f":robot: | Autopromotions started by <@{interaction.user.id}>. Conducting start-up validation checks.")
-    await log_channel.send(f":mag: | Checking for AAHQ, Battlefront, Guild and Recruit roles.")
+async def CheckRolesExist(log_channel) -> None:   
+    guild = GO.BOT_CLIENT.get_guild(AB_SERVER_ID)
 
-    guild = GO.BOT_CLIENT.get_guild(AB_SERVER)
-
-    for k, v in ROLE_IDS.items():
-        r = guild.get_role(v)
-        if r is not None:
-            await log_channel.send(f":white_check_mark | Role {r} exists.")
+    for role_name, role_id in ROLE_IDS.items():
+        role = guild.get_role(role_id)
+        if role is None:
+            await log_channel.send(f"{MSGS['rolenotexist'] + MSGS['createrole']}".format(**locals(), **globals()))
+            raise ValueError("foo2")
         else:
-            await log_channel.send(f":x: | Role {r} does not exist.")
+            await log_channel.send(f"{MSGS['roleexists']}".format(**locals(), **globals()))
 
